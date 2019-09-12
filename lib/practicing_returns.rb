5d15cbeeab3db52c02383aeb501bd22497ec8230
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-12 04:05:11 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,11 +1,13 @@
-require 'pry'
+
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
-    i += 1
+    collection << yield(array[i])
+    i = i.next
   end
+  collection
 end
 
 
